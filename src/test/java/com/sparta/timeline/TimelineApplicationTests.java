e6fef12a74326ef3086770fac8777a93f77d2717
jsy <jjungseoyeon@naver.com>
2021-08-10 20:32:00 +0900

    Initial commit

---
+++ src/test/java/com/sparta/timeline/TimelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.timeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
