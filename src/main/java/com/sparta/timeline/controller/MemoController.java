0adf9828b6133cad241f196adb93f925674fdc00
jsy <jjungseoyeon@naver.com>
2021-08-11 17:16:37 +0900

    create controller

---
+++ src/main/java/com/sparta/timeline/controller/MemoController.java
@@ -0,0 +1,41 @@
+package com.sparta.timeline.controller;
+
+import com.sparta.timeline.domain.Memo;
+import com.sparta.timeline.domain.MemoRepository;
+import com.sparta.timeline.domain.MemoRequestDto;
+import com.sparta.timeline.service.MemoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class MemoController {
+
+    private final MemoRepository memoRepository;
+    private final MemoService memoService;
+
+    @PostMapping("/api/memos")
+    public Memo createMemo(@RequestBody MemoRequestDto requestDto) {
+        Memo memo = new Memo(requestDto);
+        return memoRepository.save(memo);
+    }
+
+    @GetMapping("/api/memos")
+    public List<Memo> readMemo() {
+        return memoRepository.findAllByOrderByModifiedAtDesc();
+    }
+
+    @PutMapping("/api/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        memoService.update(id, requestDto);
+        return id;
+    }
+
+    @DeleteMapping("/api/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        memoRepository.deleteById(id);
+        return id;
+    }
+}
